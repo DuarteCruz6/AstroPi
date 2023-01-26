caaf35c60b76f658a131173cc2784b5a6b1e2885
Duarte Cruz <102768606+DuarteCruz6@users.noreply.github.com>
2023-01-26 14:51:09 +0000

    Added a new feature
    The code now reads the CPU Temperature!

--- AstroPi - Code and Data/main.py
+++ AstroPi - Code and Data/main.py
@@ -1,7 +1,7 @@
 #Imports the libraries needed
 from sense_hat import SenseHat
 from pathlib import Path
-from gpiozero import MotionSensor
+from gpiozero import MotionSensor, CPUTemperature
 import time
 from datetime import datetime, timedelta
 
@@ -14,6 +14,7 @@ now_time = datetime.now()
 pir = MotionSensor(4)
 sense = SenseHat()
 sense.clear()
+cpu = CPUTemperature()
 
 #Creates an .csv file on the location of main.py
 base_folder = Path(__file__).parent.resolve()
@@ -21,13 +22,13 @@ data_file = base_folder / "data.csv"
 
 #Variable that lets us check what's beijng shown in the LED Screen
 i = 0
-dic = ["TIME","MOTION","PRESSURE", "TEMPERATURE FROM HUMIDITY SENSOR","TEMPERATURE FROM PRESSURE SENSOR","HUMIDITY","LIGHT", "MOVEMENT X-ASSIS", "MOVEMENT Y-ASSIS", "MOVEMENT Z-ASSIS", "ACCELERATION X-ASSIS", "ACCELERATION Y-ASSIS", "ACCELERATION Z-ASSIS"]
+dic = ["TIME","MOTION","PRESSURE", "TEMPERATURE FROM HUMIDITY SENSOR","TEMPERATURE FROM PRESSURE SENSOR","CPU TEMPERATURE","HUMIDITY","LIGHT", "MOVEMENT X-ASSIS", "MOVEMENT Y-ASSIS", "MOVEMENT Z-ASSIS", "ACCELERATION X-ASSIS", "ACCELERATION Y-ASSIS", "ACCELERATION Z-ASSIS"]
 x = timedelta(hours = 2, minutes = 59, seconds = 42) 
-loopTime = {0:15, 1:6, 2:8, 3:18, 4:19, 5:7, 6:11, 7:12, 8:12, 9:12, 10:13, 11:13, 12:13} #How much it takes to each data + value to show on the LED matrix
+loopTime = {0:15, 1:6, 2:8, 3:18, 4:19, 5:13, 6:7, 7:11, 8:12, 9:12, 10:12, 11:13, 12:13, 13:13} #How much it takes to each data + value to show on the LED matrix
 
 #Writes an Header on the .csv file
 with open (data_file, "w", buffering = 1) as f:
-   f.write("TIME, MOTION (YES/NO), PRESSURE, TEMPERATURE FROM HUMIDITY SENSOR,TEMPERATURE FROM PRESSURE SENSOR,HUMIDITY,LIGHT, MOVEMENT X-ASSIS, MOVEMENT Y-ASSIS, MOVEMENT Z-ASSIS, ACCELERATION X-ASSIS, ACCELERATION Y-ASSIS, ACCELERATION Z-ASSIS")
+   f.write("TIME, MOTION (YES/NO), PRESSURE, TEMPERATURE FROM HUMIDITY SENSOR,TEMPERATURE FROM PRESSURE SENSOR,CPU TEMPERATURE, HUMIDITY,LIGHT, MOVEMENT X-ASSIS, MOVEMENT Y-ASSIS, MOVEMENT Z-ASSIS, ACCELERATION X-ASSIS, ACCELERATION Y-ASSIS, ACCELERATION Z-ASSIS")
    f.write("\n")
 
 #Loop that runs for 3 hours      
@@ -36,7 +37,7 @@ while now_time - start_time < x :
     def show():                                        #Function that changes the data on the LED screen if the astronaut presses the joystick
         global i
         i+= 0.5
-        if i == 13:
+        if i == 14:
             i = 0
 
     if pir.value == 1:                                   #Motion
@@ -46,6 +47,7 @@ while now_time - start_time < x :
     p = sense.get_pressure()                             #Pressure
     t_h = sense.get_temperature()                        #Temperature in the humidity sensor
     t_p = sense.get_temperature_from_pressure()          #Temperature in the pressure sensor
+    t_cpu = cpu.temperature                              #CPU Temperature
     h = sense.get_humidity()                             #Humidity
     sense.color.gain = 60                                #Light
     sense.color.integration_cycles = 64                  #Light
@@ -62,11 +64,11 @@ while now_time - start_time < x :
                                            
     with open (data_file, "a", buffering = 1) as f:     #Writing the data on the .csv file
         
-        f.write(f"{time},{m}, {p}, {t_h}, {t_p}, {h}, {l}, {x}, {y}, {z}, {a_x}, {a_y}, {a_z}")
+        f.write(f"{time},{m}, {p}, {t_h}, {t_p}, {t_cpu}, {h}, {l}, {x}, {y}, {z}, {a_x}, {a_y}, {a_z}")
         f.write("\n")
     
     sense.stick.direction_any = show                    #If the astronaut moves the joystick, he wants to check some value on the LED's
-    values = {"0": time, "1":m, "2":p, "3":t_h, "4":t_p,"5":h,"6":l, "7":x,"8":y,"9":z,"10":a_x,"11":a_y,"12":a_z}
+    values = {"0": time, "1":m, "2":p, "3":t_h, "4":t_p, "5":t_cpu, "6":h, "7":l, "8":x, "9":y, "10":z, "11":a_x, "12":a_y, "13":a_z}
     value = values[str(int(i))]                        #Corresponds the data to the joystick
 
     if type(value) == float:
@@ -84,4 +86,4 @@ while now_time - start_time < x :
     x = timedelta(hours = 2, minutes = 59, seconds = 60- loopTime[i])                 #Changes the time of the loop
     sense.show_message(dic[int(i)] + " " + str(value) ,scroll_speed = 0.08)        #Shows the data on the LED screen      
     now_time = datetime.now()
-  
+
